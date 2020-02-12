5466d59529069538a9e5a7afc09d86f8fd04f94b
nstabel <n.g.stabel@hotmail.com>
2020-02-12 15:39:17 +0100

    Form basic working structure for project
    Use this commit to see how the structure of the project would work.

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/04 17:31:03 by nstabel        #+#    #+#                */
-/*   Updated: 2020/02/12 13:35:20 by nstabel       ########   odam.nl         */
+/*   Updated: 2020/02/12 14:20:48 by nstabel       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void			malloc_transition_table(t_machine **machine)
 {
-	(*machine)->state = uninstall_machine;
-	(*machine)->transition = (t_state **)malloc(sizeof(t_state *) * ((*machine)->state + 1));
-	while ((*machine)->state >= idle)
+	(*machine)->state = idle;
+	(*machine)->transition = (t_state **)malloc(sizeof(t_state *) * (uninstall_machine + 1));
+	while ((*machine)->state <= uninstall_machine)
 	{
 		(*machine)->transition[(*machine)->state] = (t_state *)malloc(sizeof(t_state) * 2);
-		--(*machine)->state;
+		++(*machine)->state;
 	}
 }
 
@@ -32,11 +32,11 @@ void			fill_transition_table(t_machine **machine)
 	(*machine)->transition[receive_input][0] = print_output;
 	(*machine)->transition[receive_input][1] = validate_input;
 	(*machine)->transition[validate_input][0] = print_output;
-	(*machine)->transition[validate_input][1] = idle;
+	(*machine)->transition[validate_input][1] = print_output;
 	(*machine)->transition[print_output][0] = print_output;
 	(*machine)->transition[print_output][1] = uninstall_machine;
 	(*machine)->transition[uninstall_machine][0] = idle;
-	(*machine)->transition[uninstall_machine][1] = uninstall_machine;
+	(*machine)->transition[uninstall_machine][1] = idle;
 }
 
 void			install_transitions(t_machine **machine)
@@ -71,38 +71,43 @@ void			install(t_machine **machine)
 	*machine = (t_machine *)malloc(sizeof(t_machine));
 
 	install_transitions(machine);
-	install_transitions(machine);
+	install_events(machine);
 }
 
 int								receiving_input(void *pointer)
 {
 	pointer = NULL;
-	return (0);
-}	
+	ft_printf("receiving_input\n");
+	return (1);
+}
 
 int								validating_input(void *pointer)
 {
 	pointer = NULL;
-	return (0);
-}	
+	ft_printf("validating_input\n");
+	return (1);
+}
 
 int								printing_output(void *pointer)
 {
 	pointer = NULL;
-	return (0);
-}	
+	ft_printf("printing_output\n");
+	return (1);
+}
 
 int								uninstalling_machine(void *pointer)
 {
 	pointer = NULL;
-	return (0);
-}	
+	ft_printf("uninstalling_machine\n");
+	return (1);
+}
 
 int								ending_program(void *pointer)
 {
 	pointer = NULL;
-	return (0);
-}			
+	ft_printf("ending_program\n");
+	return (1);
+}
 
 int				main(int argc, char **argv)
 {
@@ -111,10 +116,9 @@ int				main(int argc, char **argv)
 	argc = 0;
 	argv = NULL;
 	install(&machine);
+	machine->state = receive_input;
 	while (machine->state != idle)
-	{
-		++machine->state;
-	}
+		machine->state = machine->transition[machine->state][machine->event[machine->state](machine)];
 	free(machine);
 	machine = NULL;
 	return (0);
