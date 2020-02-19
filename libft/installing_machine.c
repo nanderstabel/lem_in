9f48d3c27a87e8743b2d9a35e4950d832a77f9a9
nstabel <n.g.stabel@hotmail.com>
2020-02-19 10:48:45 +0100

    Add even more 'machine' functionality to the machine folder
    machine configuration functions are now easier to look at, and can be copy-pasted to other substates in the machine

--- libft/installing_machine.c
+++ libft/installing_machine.c
@@ -6,7 +6,7 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/18 14:08:27 by nstabel        #+#    #+#                */
-/*   Updated: 2020/02/18 14:44:52 by nstabel       ########   odam.nl         */
+/*   Updated: 2020/02/19 10:39:20 by nstabel       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@
 ** installed.
 */
 
-t_bool			installing_machine(t_machine **machine, t_input *input)
+t_bool			installing_machine(t_machine **machine, t_mconfig *mconfig)
 {
 	*machine = (t_machine *)malloc(sizeof(t_machine));
-	if (*machine == NULL || input == NULL)
+	if (*machine == NULL || mconfig == NULL)
 		return (FAIL);
-	if (input->transitions == NULL || input->events == NULL)
+	if (mconfig->transitions == NULL || mconfig->events == NULL)
 		return (FAIL);
 	(*machine)->current_state = 0;
-	(*machine)->size = input->size;
-	(*machine)->transition_table = input->transitions;
-	(*machine)->event = input->events;
+	(*machine)->size = mconfig->size;
+	(*machine)->transition_table = mconfig->transitions;
+	(*machine)->event = mconfig->events;
 	(*machine)->transition = SUCCESS;
 	return (SUCCESS);
 }
