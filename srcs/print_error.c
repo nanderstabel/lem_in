0a4916884eee63cf061efee29b10aa0925e42151
nstabel <n.g.stabel@hotmail.com>
2020-06-12 15:56:59 +0200

    Remove error message

--- srcs/print_error.c
+++ srcs/print_error.c
@@ -6,7 +6,7 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/16 10:41:21 by nstabel       #+#    #+#                 */
-/*   Updated: 2020/06/08 17:39:19 by zitzak        ########   odam.nl         */
+/*   Updated: 2020/06/12 15:53:48 by nstabel       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_bool								print_error(t_project *lem_in)
 {
 	if (lem_in->flags & DEBUG_O)
 		ft_printf("%s\n", __func__);
+	if (**lem_in->argv)
+		return (SUCCESS);
 	if (lem_in->error)
 		ft_dprintf(1, "ERROR\n");
 	if (lem_in->flags & ERROR_O)
